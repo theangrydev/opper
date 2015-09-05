8923ae8fef0a747af23dc38a2357ba9b4bcc436f
Liam Williams <liam.williams@zoho.com>
2015-09-05 18:14:12 +0100

    Move example grammar to test package.

--- src/test/java/io/github/theangrydev/opper/ParserTest.java
+++ src/test/java/io/github/theangrydev/opper/ParserTest.java
@@ -32,4 +32,36 @@ public class ParserTest {
 			return symbols.get(index);
 		}
 	}
+
+	private static class ExampleGrammar implements Grammar {
+		private static final Symbol DUMMY = new Symbol(4, "DUMMY");
+		private static final Symbol SECOND = new Symbol(3, "SECOND");
+		private static final Symbol ACCEPT = new Symbol(1, "ACCEPT");
+		private static final Symbol FIRST = new Symbol(2, "FIRST");
+		private static final Symbol START = new Symbol(0, "START");
+
+		private static final Rule ACCEPT_RULE = new Rule(0, ACCEPT, new String(SECOND, DUMMY));
+		private static final Rule FIRST_TO_ACCEPT = new Rule(2, FIRST, new String(ACCEPT));
+		private static final Rule START_TO_FIRST = new Rule(1, START, new String(FIRST));
+
+		@Override
+		public List<Symbol> symbols() {
+			return Arrays.asList(START, ACCEPT, FIRST, SECOND, DUMMY);
+		}
+
+		@Override
+		public List<Rule> rules() {
+			return Arrays.asList(ACCEPT_RULE, FIRST_TO_ACCEPT);
+		}
+
+		@Override
+		public Symbol acceptanceSymbol() {
+			return ACCEPT;
+		}
+
+		@Override
+		public Rule startRule() {
+			return START_TO_FIRST;
+		}
+	}
 }

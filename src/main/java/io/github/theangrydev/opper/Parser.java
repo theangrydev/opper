c3f1f4ccc2a56be920d50d1e466890131a695278
Liam Williams <liam.williams@zoho.com>
2015-09-09 20:04:39 +0100

    Refactoring loop variable as a field so that it is not passed around everywhere

--- src/main/java/io/github/theangrydev/opper/Parser.java
+++ src/main/java/io/github/theangrydev/opper/Parser.java
@@ -15,6 +15,7 @@ public class Parser {
 	private final EarlyItemFactory earlyItemFactory;
 	private final TransitionTables transitionTables;
 	private final EarlySetsTable earlySetsTable;
+	private int currentEarlySetIndex;
 
 	public Parser(Logger logger, Grammar grammar, Corpus corpus) {
 		this.logger = logger;
@@ -26,10 +27,10 @@ public class Parser {
 		this.transitionTables = new TransitionTables(grammar);
 	}
 
-	private void debug(int i) {
+	private void debug() {
 		logger.log(() -> "");
-		logger.log(() -> "State at end of iteration #" + i);
-		logger.log(() -> "Early set [" + i + "]: " + earlySet(i));
+		logger.log(() -> "State at end of iteration #" + currentEarlySetIndex);
+		logger.log(() -> "Early set [" + currentEarlySetIndex + "]: " + currentEarlySet());
 		logger.log(() -> "Transition tables: " + transitionTables);
 		logger.log(() -> "");
 	}
@@ -37,15 +38,15 @@ public class Parser {
 	// Algorithm 1
 	public boolean parse() {
 		initialize();
-		for (int i = 1; corpus.hasNextSymbol(); i++) {
+		for (currentEarlySetIndex = 1; corpus.hasNextSymbol(); currentEarlySetIndex++) {
 			expand();
-			scan(i);
-			if (earlySet(i).isEmpty()) {
+			scan();
+			if (currentEarlySet().isEmpty()) {
 				logger.log(() -> "The early set was empty after scanning, failing the parse.");
 				return false;
 			}
-			reduce(i);
-			debug(i);
+			reduce();
+			debug();
 		}
 		return lastEarlySetHasCompletedAcceptanceRule();
 	}
@@ -66,53 +67,53 @@ public class Parser {
 	// Algorithm 2
 	private void initialize() {
 		expand();
-		addEarlyItem(0, DottedRule.begin(grammar.acceptanceRule()), 0);
-		reduce(0);
-		debug(0);
+		addEarlyItem(DottedRule.begin(grammar.acceptanceRule()), 0);
+		reduce();
+		debug();
 	}
 
 	// Algorithm 3
-	private void scan(int i) {
-		logger.log(() -> "Scan #" + i);
+	private void scan() {
+		logger.log(() -> "Scan #" + currentEarlySetIndex);
 		Symbol symbol = corpus.nextSymbol();
 		logger.log(() -> "Processing symbol: " + symbol);
-		Set<EarlyOrLeoItem> predecessors = transitionEarlySet(i - 1, symbol);
-		logger.log(() -> "Predecessors in transition early set #" + (i - 1) + ": " + predecessors);
+		Set<EarlyOrLeoItem> predecessors = previousTransitionsEarlySet(symbol);
+		logger.log(() -> "Predecessors in transition early set #" + (currentEarlySetIndex - 1) + ": " + predecessors);
 		for (EarlyOrLeoItem predecessor : predecessors) {
 			int origin = predecessor.origin();
 			logger.log(() -> "origin: " + origin);
 			DottedRule next = predecessor.transition(symbol);
 			logger.log(() -> "next: " + next);
-			addEarlyItem(i, next, origin);
+			addEarlyItem(next, origin);
 		}
 	}
 
 	// Algorithm 4
-	private void reduce(int i) {
-		logger.log(() -> "Reduce #" + i);
-		for (EarlyItem earlyItem : earlySet(i)) {
+	private void reduce() {
+		logger.log(() -> "Reduce #" + currentEarlySetIndex);
+		for (EarlyItem earlyItem : currentEarlySet()) {
 			logger.log(() -> "Processing: " + earlyItem);
 			int origin = earlyItem.origin();
 			Optional<Symbol> left = earlyItem.leftOfCompletedRule();
 			logger.log(() -> "Left of completed rule: " + left);
 			if (left.isPresent()) {
-				reduceOneLeft(i, origin, left.get());
+				reduceOneLeft(origin, left.get());
 			}
 		}
-		memoizeTransitions(i);
+		memoizeTransitions();
 	}
 
 	// Algorithm 5
-	private void memoizeTransitions(int i) {
-		for (EarlyItem earlyItem : earlySet(i)) {
+	private void memoizeTransitions() {
+		for (EarlyItem earlyItem : currentEarlySet()) {
 			DottedRule dottedRule = earlyItem.dottedRule();
 			if (dottedRule.isComplete()) {
 				continue;
 			}
 			Symbol postdot = dottedRule.postDot();
-			Set<EarlyOrLeoItem> transitions = transitionEarlySet(i, postdot);
+			Set<EarlyOrLeoItem> transitions = currentTransitionsEarlySet(postdot);
 			if (postdotTransitionIsUniqueByRightRecursiveRule(dottedRule)) {
-				transitions.add(new LeoItem(dottedRule, postdot, i));
+				transitions.add(new LeoItem(dottedRule, postdot, currentEarlySetIndex));
 			} else {
 				transitions.add(earlyItem);
 			}
@@ -120,41 +121,41 @@ public class Parser {
 	}
 
 	// Algorithm 6
-	private void reduceOneLeft(int i, int origin, Symbol left) {
+	private void reduceOneLeft(int origin, Symbol left) {
 		logger.log(() -> "Reduce one");
 		Set<EarlyOrLeoItem> transitionEarlySet = transitionEarlySet(origin, left);
 		logger.log(() -> "Origin transitions[" + origin + "," + left + "]: " + transitionEarlySet);
 		for (EarlyOrLeoItem item : transitionEarlySet) {
-			performEarlyReduction(i, left, item);
+			performEarlyReduction(left, item);
 		}
 	}
 
-	private void performEarlyReduction(int i, Symbol left, EarlyOrLeoItem item) {
+	private void performEarlyReduction(Symbol left, EarlyOrLeoItem item) {
 		// Algorithm 7 and 8 are handled using polymorphism
 		DottedRule next = item.transition(left);
 		int origin = item.origin();
-		addEarlyItem(i, next, origin);
+		addEarlyItem(next, origin);
 	}
 
 	// Algorithm 9
-	private void addEarlyItem(int i, DottedRule confirmed, int origin) {
-		logger.log(() -> "Adding early item to set #" + i + " with rule " + confirmed + " and origin " + origin);
+	private void addEarlyItem(DottedRule confirmed, int origin) {
+		logger.log(() -> "Adding early item to set #" + currentEarlySetIndex + " with rule " + confirmed + " and origin " + origin);
 		EarlyItem confirmedEarlyItem = earlyItemFactory.createEarlyItem(confirmed, origin);
-		EarlySet earlySet = earlySet(i);
-		addEarlyItemIfItIsNew(i, earlySet, confirmedEarlyItem);
+		EarlySet earlySet = currentEarlySet();
+		addEarlyItemIfItIsNew(earlySet, confirmedEarlyItem);
 		if (confirmed.isComplete()) {
 			logger.log(() -> "Dotted rule is complete, not doing any predictions.");
 			return;
 		}
 		logger.log(() -> "Making predictions based on the postdot symbol: " + confirmed.postDot());
 		for (Rule rule : rulePrediction.predict(confirmed.postDot())) {
-			EarlyItem earlyItem = earlyItemFactory.createEarlyItem(rule, i);
-			addEarlyItemIfItIsNew(i, earlySet, earlyItem);
+			EarlyItem earlyItem = earlyItemFactory.createEarlyItem(rule, currentEarlySetIndex);
+			addEarlyItemIfItIsNew(earlySet, earlyItem);
 		}
 	}
 
-	private void addEarlyItemIfItIsNew(int i, EarlySet earlySet, EarlyItem earlyItem) {
-		if (isNew(i, earlyItem)) {
+	private void addEarlyItemIfItIsNew(EarlySet earlySet, EarlyItem earlyItem) {
+		if (isNew(earlyItem)) {
 			logger.log(() -> "Early item is new, adding it...");
 			earlySet.add(earlyItem);
 		} else {
@@ -166,15 +167,27 @@ public class Parser {
 		return false;
 	}
 
-	private boolean isNew(int i, EarlyItem earlyItem) {
-		return earlySet(earlyItem.origin()).isNew(i, earlyItem);
+	private boolean isNew(EarlyItem earlyItem) {
+		return earlySet(earlyItem.origin()).isNew(currentEarlySetIndex, earlyItem);
 	}
 
-	private EarlySet earlySet(int i) {
-		return earlySetsTable.earlySet(i);
+	private EarlySet earlySet(int earlySetIndex) {
+		return earlySetsTable.earlySet(earlySetIndex);
 	}
 
-	private Set<EarlyOrLeoItem> transitionEarlySet(int i, Symbol symbol) {
-		return transitionTables.transitions(symbol, i);
+	private EarlySet currentEarlySet() {
+		return earlySet(currentEarlySetIndex);
+	}
+
+	private Set<EarlyOrLeoItem> transitionEarlySet(int earlySetIndex, Symbol symbol) {
+		return transitionTables.transitions(symbol, earlySetIndex);
+	}
+
+	private Set<EarlyOrLeoItem> currentTransitionsEarlySet(Symbol postdot) {
+		return transitionEarlySet(currentEarlySetIndex, postdot);
+	}
+
+	private Set<EarlyOrLeoItem> previousTransitionsEarlySet(Symbol symbol) {
+		return transitionEarlySet(currentEarlySetIndex - 1, symbol);
 	}
 }

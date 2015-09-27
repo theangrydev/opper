8fec78ca58e7a1e4de02418e691d9ffee55f09ae
Liam Williams <liam.williams@zoho.com>
2015-09-27 17:08:24 +0100

    Unit test for BDDScanner

--- src/test/java/io/github/theangrydev/opper/scanner/definition/BDDScannerTest.java
+++ src/test/java/io/github/theangrydev/opper/scanner/definition/BDDScannerTest.java
@@ -2,7 +2,11 @@ package io.github.theangrydev.opper.scanner.definition;
 
 import io.github.theangrydev.opper.grammar.Symbol;
 import io.github.theangrydev.opper.scanner.autonoma.BDDScanner;
+import org.assertj.core.api.WithAssertions;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.github.theangrydev.opper.scanner.definition.AlternativeExpression.either;
 import static io.github.theangrydev.opper.scanner.definition.CharacterExpression.character;
@@ -10,17 +14,24 @@ import static io.github.theangrydev.opper.scanner.definition.ConcatenateExpressi
 import static io.github.theangrydev.opper.scanner.definition.RepeatExpression.repeat;
 import static java.util.Collections.singletonList;
 
-public class BDDScannerTest {
+public class BDDScannerTest implements WithAssertions {
 
 	@Test
-	public void shouldScan() {
+	public void shouldScanASimpleExpression() {
 		Expression expression = repeat(either(character('a'), character('b'), concatenate(character('c'), character('d'))));
-		SymbolDefinition symbolDefinition = new SymbolDefinition(new Symbol(1, "symbol"), expression);
+		Symbol symbol = new Symbol(1, "symbol");
+		SymbolDefinition symbolDefinition = new SymbolDefinition(symbol, expression);
 
-		//TODO: this is broken for repeated inputs and other stuff
-		BDDScanner bddScanner = new BDDScanner(singletonList(symbolDefinition), 'c', 'd', 'c', 'd');
+		BDDScanner bddScanner = new BDDScanner(singletonList(symbolDefinition), 'c', 'd', 'c', 'd', 'a', 'b');
+
+		assertThat(allSymbolsThatCanBeScanned(bddScanner)).containsOnly(symbol).hasSize(4);
+	}
+
+	private List<Symbol> allSymbolsThatCanBeScanned(BDDScanner bddScanner) {
+		List<Symbol> accepted = new ArrayList<>();
 		while (bddScanner.hasNextSymbol()) {
-			System.out.println("next=" + bddScanner.nextSymbol());
+			accepted.add(bddScanner.nextSymbol());
 		}
+		return accepted;
 	}
 }

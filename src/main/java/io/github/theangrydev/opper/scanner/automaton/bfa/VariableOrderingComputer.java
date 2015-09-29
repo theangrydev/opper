21c9147b60c05633bf8925bb680b62bfd15c1e19
Liam Williams <liam.williams@zoho.com>
2015-09-29 23:39:13 +0100

    Remove unused code

--- src/main/java/io/github/theangrydev/opper/scanner/automaton/bfa/VariableOrderingComputer.java
+++ src/main/java/io/github/theangrydev/opper/scanner/automaton/bfa/VariableOrderingComputer.java
@@ -20,15 +20,17 @@ public class VariableOrderingComputer {
 		List<VariableOrder> variableOrders = new ArrayList<>(bitsPerRow);
 		List<TransitionTable> frontier = singletonList(transitionTable);
 		for (int height = 0; height < bitsPerRow; height++) {
-			int countPerSplit = 1 << (bitsPerRow - height - 1);
+			int countPerSplit = countPerSplit(bitsPerRow, height);
 			int nextVariable = determineNext(frontier, remainingVariableIds, countPerSplit);
 			remainingVariableIds.remove(nextVariable);
 			variableOrders.add(new VariableOrder(height, nextVariable));
 			frontier = nextFrontier(frontier, nextVariable);
 		}
-		DecisionTree tree = DecisionTree.from(transitionTable, variableOrders);
-		System.out.println("nodes=" + tree.count());
 		return new VariableOrdering(variableSummary, variableOrders);
+	}
+
+	private static int countPerSplit(int bitsPerRow, int height) {
+		return 1 << (bitsPerRow - height - 1);
 	}
 
 	private static List<TransitionTable> nextFrontier(List<TransitionTable> frontier, int nextVariable) {
@@ -68,49 +70,5 @@ public class VariableOrderingComputer {
 		double positiveProportion = (double) positiveExamples / countPerSplit;
 		double negativeProportion = (double) negativeExamples / countPerSplit;
 		return positiveProportion * log2(positiveProportion) + negativeProportion * log2(negativeProportion);
-	}
-
-	private static class DecisionTree {
-		private TransitionTable examples;
-		private DecisionTree zero;
-		private DecisionTree one;
-
-		public static DecisionTree from(TransitionTable transitionTable, List<VariableOrder> variableOrderOrdering) {
-			DecisionTree root = new DecisionTree(transitionTable);
-			List<DecisionTree> trees = new ArrayList<>();
-			trees.add(root);
-			List<DecisionTree> nextTrees = new ArrayList<>();
-			for (VariableOrder variableOrder : variableOrderOrdering) {
-				for (DecisionTree tree : trees) {
-					if (tree.examples.isEmpty()) {
-						continue;
-					}
-					DecisionTree one = new DecisionTree(tree.examples.rowsWithVariable(variableOrder.id()));
-					DecisionTree zero = new DecisionTree(tree.examples.rowsWithoutVariable(variableOrder.id()));
-					tree.one = one;
-					tree.zero = zero;
-					nextTrees.add(one);
-					nextTrees.add(zero);
-				}
-				trees = nextTrees;
-				nextTrees = new ArrayList<>();
-			}
-			return root;
-		}
-
-		private DecisionTree(TransitionTable examples) {
-			this.examples = examples;
-		}
-
-		public int count() {
-			int count = 1;
-			if (zero != null) {
-				count += zero.count();
-			}
-			if (one != null) {
-				count += one.count();
-			}
-			return count;
-		}
 	}
 }

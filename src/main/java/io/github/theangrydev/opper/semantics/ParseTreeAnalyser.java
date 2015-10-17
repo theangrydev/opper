3339fb3b42129d09249c92ee266eaf3440c2f241
Liam Williams <liam.williams@zoho.com>
2015-10-17 23:32:27 +0100

    Move semantics classes from opper to op

--- src/main/java/io/github/theangrydev/opper/semantics/ParseTreeAnalyser.java
+++
@@ -1,7 +0,0 @@
-package io.github.theangrydev.opper.semantics;
-
-import io.github.theangrydev.opper.parser.tree.ParseTree;
-
-public interface ParseTreeAnalyser<Result> {
-	Result analyse(ParseTree parseTree);
-}

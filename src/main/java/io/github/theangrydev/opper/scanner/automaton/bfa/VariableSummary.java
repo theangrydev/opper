eb9c73ed107792b9d5f9f782c2dc7c5db4e4a673
Liam Williams <liam.williams@zoho.com>
2015-11-15 18:05:02 +0000

    Adding copyright headers

--- src/main/java/io/github/theangrydev/opper/scanner/automaton/bfa/VariableSummary.java
+++ src/main/java/io/github/theangrydev/opper/scanner/automaton/bfa/VariableSummary.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2015 Liam Williams <liam.williams@zoho.com>.
+ *
+ * This file is part of opper.
+ *
+ * opper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * opper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with opper.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.github.theangrydev.opper.scanner.automaton.bfa;
 
 import com.google.common.math.IntMath;

2bd1fd36eaaae60426f388c1ec03b915c205d342
Liam Williams <liam.williams@zoho.com>
2017-01-14 22:06:03 +0000

    Test coverage for ambiguous parses

---
+++ src/test/java/io/github/theangrydev/opper/parser/AmbiguousParserTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015-2016 Liam Williams <liam.williams@zoho.com>.
+ *
+ * This file is part of opper.
+ *
+ * opper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * opper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with opper.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.theangrydev.opper.parser;
+
+import io.github.theangrydev.opper.common.DoNothingLogger;
+import io.github.theangrydev.opper.grammar.Grammar;
+import io.github.theangrydev.opper.grammar.GrammarBuilder;
+import io.github.theangrydev.opper.scanner.FixedScanner;
+import io.github.theangrydev.opper.scanner.Scanner;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AmbiguousParserTest {
+
+    @Test
+    public void shouldRecogniseAnAmbiguousGrammar() throws IOException {
+        Grammar grammar = new GrammarBuilder()
+                .withAcceptanceSymbol("ACCEPT")
+                .withStartSymbol("START")
+                .withRule("START", "A")
+                .withRule("START", "START", "PLUS", "START")
+                .build();
+        Scanner scanner = FixedScanner.scanner(grammar, "A", "PLUS", "A", "PLUS", "A");
+
+        Parser parser = new EarlyParserFactory(new DoNothingLogger(), grammar).parser(scanner);
+
+        assertThat(parser.parse()).isPresent();
+    }
+}

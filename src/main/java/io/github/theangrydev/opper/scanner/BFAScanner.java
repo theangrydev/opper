eb81233b2eb4d1d40e1e59b747211dbcb314e97c
Liam Williams <liam.williams@zoho.com>
2017-01-14 10:07:05 +0000

    Indentation changes

--- src/main/java/io/github/theangrydev/opper/scanner/BFAScanner.java
+++ src/main/java/io/github/theangrydev/opper/scanner/BFAScanner.java
@@ -29,90 +29,89 @@ import static io.github.theangrydev.opper.scanner.ScannedSymbol.scannedSymbol;
 
 public class BFAScanner implements Scanner {
 
-	private static final char READER_END_OF_INPUT_MARKER = '\uFFFF'; // this is the result of (char) -1
+    private static final char READER_END_OF_INPUT_MARKER = '\uFFFF'; // this is the result of (char) -1
 
-	private final Reader charactersToParse;
-	private final BFA bfa;
+    private final Reader charactersToParse;
+    private final BFA bfa;
 
-	private PositionTracker positionTracker = new PositionTracker();
-	private char character;
-	private StringBuilder symbolCharacters;
+    private PositionTracker positionTracker = new PositionTracker();
+    private char character;
+    private StringBuilder symbolCharacters;
 
-	private BFAScanner(BFA bfa, Reader charactersToParse, char firstCharacter) {
-		this.bfa = bfa;
-		this.charactersToParse = charactersToParse;
-		this.positionTracker = new PositionTracker();
-		this.character = firstCharacter;
-		this.symbolCharacters = new StringBuilder();
-	}
+    private BFAScanner(BFA bfa, Reader charactersToParse, char firstCharacter) {
+        this.bfa = bfa;
+        this.charactersToParse = charactersToParse;
+        this.positionTracker = new PositionTracker();
+        this.character = firstCharacter;
+        this.symbolCharacters = new StringBuilder();
+    }
 
-	public static BFAScanner createBFAScanner(BFA bfa, Reader charactersToParse) throws IOException {
-		char firstCharacter = (char) charactersToParse.read();
-		return new BFAScanner(bfa, charactersToParse, firstCharacter);
-	}
+    public static BFAScanner createBFAScanner(BFA bfa, Reader charactersToParse) throws IOException {
+        char firstCharacter = (char) charactersToParse.read();
+        return new BFAScanner(bfa, charactersToParse, firstCharacter);
+    }
 
-	@Override
-	public ScannedSymbol nextSymbol() throws IOException {
-		positionTracker.markSymbolStart();
-		symbolCharacters.setLength(0);
+    @Override
+    public ScannedSymbol nextSymbol() throws IOException {
+        positionTracker.markSymbolStart();
+        symbolCharacters.setLength(0);
 
-		BinaryDecisionDiagram nextFrontier = bfa.initialState();
-		BinaryDecisionDiagram frontier;
+        BinaryDecisionDiagram nextFrontier = bfa.initialState();
+        BinaryDecisionDiagram frontier;
 
-		do {
-			frontier = nextFrontier.copy();
-			nextFrontier = bfa.transition(nextFrontier, character);
+        do {
+            frontier = nextFrontier.copy();
+            nextFrontier = bfa.transition(nextFrontier, character);
 
             if (!nextFrontier.isZero()) {
                 positionTracker.consider(character);
                 symbolCharacters.append(character);
                 frontier.discard();
-				character = (char) charactersToParse.read();
+                character = (char) charactersToParse.read();
             }
         } while (!nextFrontier.isZero());
 
-		if (symbolCharacters.length() == 0) {
-			throw new UnsupportedOperationException("TODO: handle character sequences that are not scannable");
-		}
+        if (symbolCharacters.length() == 0) {
+            throw new UnsupportedOperationException("TODO: handle character sequences that are not scannable");
+        }
 
-		Symbol acceptingSymbol = bfa.acceptingSymbol(frontier);
-		frontier.discard();
-		return scannedSymbol(acceptingSymbol, symbolCharacters.toString(), positionTracker.currentLocation());
-	}
+        Symbol acceptingSymbol = bfa.acceptingSymbol(frontier);
+        frontier.discard();
+        return scannedSymbol(acceptingSymbol, symbolCharacters.toString(), positionTracker.currentLocation());
+    }
 
-	@Override
-	public boolean hasNextSymbol() {
-		return character != READER_END_OF_INPUT_MARKER;
-	}
+    @Override
+    public boolean hasNextSymbol() {
+        return character != READER_END_OF_INPUT_MARKER;
+    }
 
-	private static class PositionTracker {
-		private int currentLineNumber;
-		private int currentCharacterNumber;
-		private int markedLineNumber;
-		private int markedCharacterNumber;
+    private static class PositionTracker {
+        private int currentLineNumber;
+        private int currentCharacterNumber;
+        private int markedLineNumber;
+        private int markedCharacterNumber;
 
-		public PositionTracker() {
-			currentLineNumber = 1;
-			currentCharacterNumber = 0;
-			markedCharacterNumber = 1;
-		}
+        public PositionTracker() {
+            currentLineNumber = 1;
+            currentCharacterNumber = 0;
+            markedCharacterNumber = 1;
+        }
 
-		public void markSymbolStart() {
-			markedLineNumber = currentLineNumber;
-			markedCharacterNumber = currentCharacterNumber + 1;
-		}
+        public void markSymbolStart() {
+            markedLineNumber = currentLineNumber;
+            markedCharacterNumber = currentCharacterNumber + 1;
+        }
 
-		public void consider(char character) {
-			currentCharacterNumber++;
-			if (character == '\n') {
-				currentLineNumber++;
-				currentCharacterNumber = 0;
-			}
-		}
+        public void consider(char character) {
+            currentCharacterNumber++;
+            if (character == '\n') {
+                currentLineNumber++;
+                currentCharacterNumber = 0;
+            }
+        }
 
-		public Location currentLocation() {
-			return Location.location(markedLineNumber, markedCharacterNumber, currentLineNumber, currentCharacterNumber);
-		}
-	}
-
+        public Location currentLocation() {
+            return Location.location(markedLineNumber, markedCharacterNumber, currentLineNumber, currentCharacterNumber);
+        }
+    }
 }
